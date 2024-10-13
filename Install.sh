e99f21077821744b7bacdb5357f1f50229b125f7
Madhurendra kumar <95265573+m14r41@users.noreply.github.com>
2024-10-14 03:04:44 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,17 +1,10 @@
-#!/bin/bash
-
-# Clone the SemgrepAnalyzer repository
 git clone https://github.com/m14r41/SemgrepAnalyzer.git
 
-# Navigate into the cloned repository
 cd SemgrepAnalyzer
 
-# Rename the script and make it executable
 mv semgrepAnalyzer.py semgrepAnalyzer
 chmod +x semgrepAnalyzer
 
-# Move the script to /usr/local/bin with sudo
 sudo mv semgrepAnalyzer /usr/local/bin
 
-# Go back to the parent directory and delete the SemgrepAnalyzer folder
 cd .. && rm -rf SemgrepAnalyzer
